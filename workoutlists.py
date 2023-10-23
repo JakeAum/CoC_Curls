94d2687046e0d97273255d9bfc779d23e1f0e619
Daniel Marquez <128098411+idkwhattotype42@users.noreply.github.com>
2023-10-23 02:30:54 +0000

    leg workouts

--- workoutlists.py
+++ workoutlists.py
@@ -1,5 +1,6 @@
-#starting lists of a few possibleworkouts depending on the day and workout needs. 
-#intermediate arms excersises 
+#starting lists of a few possibleworkouts depending on the day and workout needs.
+
+#intermediate arms excersises
 armsInter = ['Dumbell biceps curl','Dumbell hammer curl'
              'Cable Rope Curl','Bench Body weight triceps dips',
              'Cable Triceps Pushdown']
@@ -9,12 +10,23 @@ armsMed = ['Push-ups','Bent over row','Lying Chest Press','Rear deltoid fly',
            'Push press','Skull Crushers','Cable Chest fly','Pull ups']
 
 #more advanced arm excerises, maybe add a prompt that says to add more reps?
-armsAdvan = ['Dead hang']
-
+armsAdvan = ['Dead hang', 'Barbell Curls','High Cable Tricep Pushdown','Overhead Bicep Cable Curls',
+             'Low Cable Bicep Curls','Barbell Overhead Tricep Extensions',
+             'Rope Tricep Extension','Alternate Dumbell Hammer Curl','Machine Preacher Curl']
+#begining of leg exervises
+legsInter = ['Body weight Squats','Leg Press','Lunges','Leg Curls','Deadlift',
+             'Step-up','Incline Treadmil Walk','Leg Extensions']
+#medium difficulty legs
+legsMed = ['Barbell Back Squat','Romainian Deadlift','Dumbbell Lunge','Goblet Squat',
+           'Standing Calf Raise','Barbell Hip Thrust','Barbell Hip Thrust']
+#advanced leg workouts
+legsAdvan = ['Stair-master','Side Lunge','Box Jump','Jumping Lunge',
+             'Bulgarian Split Squat','Sumo Squat','Weighted Donkey Calf Raise']
 
 #func to rest all code
 def run():
-   # print(armsInter)
-    return(armsAdvan)
+    print(legsInter)
+    print(legsMed)
+    return legsAdvan
 
 print(run())

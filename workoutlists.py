8808552560f1c514aba8a821be79809ad3072255
Daniel Marquez <128098411+idkwhattotype42@users.noreply.github.com>
2023-10-23 01:19:49 +0000

    medium and advanced arm workouts list created

--- workoutlists.py
+++ workoutlists.py
@@ -1,15 +1,20 @@
 #starting lists of a few possibleworkouts depending on the day and workout needs. 
 #intermediate arms excersises 
-armsInter = ['dumbell biceps curl','dumbell hammer curl'
+armsInter = ['Dumbell biceps curl','Dumbell hammer curl'
              'Cable Rope Curl','Bench Body weight triceps dips',
              'Cable Triceps Pushdown']
 #medium difficult arm exercises
 
-armsMed = []
+armsMed = ['Push-ups','Bent over row','Lying Chest Press','Rear deltoid fly',
+           'Push press','Skull Crushers','Cable Chest fly','Pull ups']
+
+#more advanced arm excerises, maybe add a prompt that says to add more reps?
+armsAdvan = ['Dead hang']
 
 
 #func to rest all code
 def run():
-    return(armsMed)
+   # print(armsInter)
+    return(armsAdvan)
 
 print(run())

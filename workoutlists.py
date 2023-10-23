f35c2b17e3b08ce1cfa969b0fde236c3021687cc
Daniel Marquez <128098411+idkwhattotype42@users.noreply.github.com>
2023-10-23 01:06:35 +0000

    first list of arms excersises

---
+++ workoutlists.py
@@ -0,0 +1,15 @@
+#starting lists of a few possibleworkouts depending on the day and workout needs. 
+#intermediate arms excersises 
+armsInter = ['dumbell biceps curl','dumbell hammer curl'
+             'Cable Rope Curl','Bench Body weight triceps dips',
+             'Cable Triceps Pushdown']
+#medium difficult arm exercises
+
+armsMed = []
+
+
+#func to rest all code
+def run():
+    return(armsMed)
+
+print(run())
